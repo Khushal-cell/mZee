24f46aa0f3fdc5f779874032f23b29674b6d21b8
Hamza Shaukat <hamzeekhattak56@gmail.com>
2022-12-17 00:18:22 +0500

    Addd Footer

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const closebtn = document.getElementById('closebtn')
 const menubtn = document.getElementById('menubtn')
 const links = document.getElementById('links')
+const messages = document.getElementById('message')
 
 menubtn.addEventListener('click', showmenu)
 closebtn.addEventListener('click', closemenu)
@@ -16,4 +17,8 @@ function closemenu(){
     links.style.top = "-100%"
     menubtn.style.display = "block"
     closebtn.style.display = "none"
+}
+
+function message(){
+  alert("Thank You! We Recieved your message.")
 }
